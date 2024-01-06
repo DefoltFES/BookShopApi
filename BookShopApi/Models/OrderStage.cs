abb9a949e7594af9a6c48d4b70499c1f94b78f6d
DefoltFES <mironoff.emil@yandex.ru>
2024-01-06 10:37:36 +0300

    Add new entities and one to many relationships

---
+++ BookShopApi/Models/OrderStage.cs
@@ -0,0 +1,15 @@
+﻿namespace BookShopApi.Models
+{
+    public class OrderStage
+    {
+        public int Id { get; set; }
+
+        public Order Order { get; set; }
+
+        public Stage Stage { get; set; }
+
+        public DateTime DateStepBeg { get; set; }
+
+        public DateTime DateStepEnd { get; set;}
+    }
+}
