7b419c6c20ef7755457cbf3d894c89b08fc173b2
DefoltFES <mironoff.emil@yandex.ru>
2024-01-06 10:13:21 +0300

    Add main entities

---
+++ BookShopApi/Models/book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookShopApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+
+        public int Amount { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
